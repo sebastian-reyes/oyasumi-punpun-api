7fc9298657e08cf0c174a71a003bb9ecd7ca4bb5
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2022-09-18 01:31:13 -0500

    Inciando Repo

---
+++ src/main/java/com/manga/punpun/PunpunApplication.java
@@ -0,0 +1,13 @@
+package com.manga.punpun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunpunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunpunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manga/punpun/PunpunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manga.punpun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunpunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
